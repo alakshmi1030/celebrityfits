3d810358603884c69197f0db3bb5b5f9a6cdb3de
Anand Lakshminarayan <anand.lakshminarayan@gmail.com>
2023-04-13 10:32:20 -0400

    Initial commit with Michael B. Jordan and Hailey Bieber

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import mbjhero from './images/michaelbjordanhero.jpeg';
+import haileybieberhero from './images/haileybieberhero.jpeg';
+import {MBJIMAGES, HAILEYBIEBERIMAGES} from './celebrityImageLists';
+
+const titleStyle = {
+    backgroundColor: "#fff",
+    border: "1px solid #000",
+    padding: "5px",
+    borderRadius: "5px"
+  };
+
+const ImageGallery = ({selectedOption}) => {
+  let images = [];
+  if(selectedOption === 'Michael B. Jordan'){
+    images = MBJIMAGES;
+  }
+  else if(selectedOption === "Hailey Bieber"){
+    images = HAILEYBIEBERIMAGES;
+  }
+  return (
+    <div className="gallery-container">
+      {images.map((image, index) => (
+        <div className="gallery-item" key={index}>
+          <a href={image.href} target="_blank"><img src={image.src} alt={`Image ${index}`} /></a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Header = ({selectedOption}) => {
+  let heroImage;
+  if(selectedOption === 'Michael B. Jordan'){
+    heroImage = mbjhero;
+  }
+  else if(selectedOption === 'Hailey Bieber'){
+    heroImage = haileybieberhero;
+  }
+
+  return (
+    <header
+      style={{ backgroundImage: `url(${heroImage})` }}
+      className="header"
+    >
+    <div style={titleStyle}>
+      <h1>{selectedOption}'s fits</h1>
+    </div>
+    </header>
+  )
+}
+
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState('Michael B. Jordan');
+  const options = ['Michael B. Jordan', 'Hailey Bieber'];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header selectedOption={selectedOption} />
+      <select value={selectedOption} onChange={e => setSelectedOption(e.target.value)}>
+        {options.map(option => (
+          <option key={option} value={option}>{option}</option>
+        ))}
+      </select>
+      <ImageGallery selectedOption={selectedOption}/>
     </div>
   );
 }
---
+++ src/celebrityImageLists.js
@@ -0,0 +1,73 @@
+import mbjcreamovercoat from './images/mbjcreamovercoat.jpeg';
+import mbjgraysweater from './images/mbjgraysweater.jpeg';
+import mbjgreenshirt from './images/mbjgreenshirt.jpeg';
+import mbjleatherjacket from './images/mbjleatherjacket.jpeg';
+import mbjnavyblazer from './images/mbjnavyblazer.jpeg';
+import mbjwhitepattern from './images/mbjwhitepattern.jpeg';
+import haileybieberblackdress from './images/haileybieberblackdress.jpeg';
+import haileybieberbrownskirt from './images/haileybieberbrownskirt.jpeg';
+import haileybiebercreamcoat from './images/haileybiebercreamcoat.jpeg';
+import haileybiebergreensuit from './images/haileybiebergreensuit.jpeg';
+import haileybieberstripedsweater from './images/haileybieberstripedsweater.jpeg';
+
+export const MBJIMAGES = [
+  {
+    src: mbjcreamovercoat,
+    alt: 'MBJ Image 1',
+    href: "https://men.expstyletrial.com/collections/coats-jackets/products/brown-water-resistant-trench-coat-in-coffee-bean"
+  },
+  {
+    src: mbjgraysweater,
+    alt: 'MBJ Image 2',
+    href: "https://www.pauljamesknitwear.com/products/mens-100-cotton-fisherman-tuck-stitch-jumper"
+  },
+  {
+    src: mbjgreenshirt,
+    alt: 'MBJ Image 3',
+    href: "https://protrending.com/product/valentino-floral-embroidered-crochet-shirt/"
+  },
+  {
+    src: mbjleatherjacket,
+    alt: 'MBJ Image 4',
+    href: "https://www.makeyourownjeans.com/jackets/overcoats/belted-leather-trench-coat"
+  },
+  {
+    src: mbjnavyblazer,
+    alt: 'MBJ Image 5',
+    href: "https://www.ebay.com/itm/234244508742?chn=ps&mkevt=1&mkcid=28&var=533850785676"
+  },
+  {
+    src: mbjwhitepattern,
+    alt: 'MBJ Image 6',
+    href: "https://danddclothing.com/products/silky-shirt-for-men-sea"
+  }
+];
+
+export const HAILEYBIEBERIMAGES = [
+  {
+    src: haileybiebercreamcoat,
+    alt: 'Hailey Bieber Image 1',
+    href: "https://www.saksfifthavenue.com/product/ena-pelly-harri-oversized-teddy-coat-0400017858539.html"
+  },
+  {
+    src: haileybieberblackdress,
+    alt: 'Hailey Bieber Image 2',
+    href: "https://www.nordstrom.com/s/milano-back-zip-sweater/7248002"
+  },
+  {
+    src: haileybieberbrownskirt,
+    alt: 'Hailey Bieber Image 3',
+    href: "https://www.aritzia.com/us/en/product/sculpt-knit-criss-cross-midi-dress/104452002.html"
+  },
+  {
+    src: haileybiebergreensuit,
+    alt: 'Hailey Bieber Image 4',
+    href: "https://www.forever21.com/us/20004736990402.html?source=shoppingads&glCountry=US&glCurrency=USD&utm_source=shoppingads&utm_medium=organic&utm_campaign=freeshopping"
+  },
+  {
+    src: haileybieberstripedsweater,
+    alt: 'Hailey Bieber Image 1',
+    href: "https://www.ladoublej.com/en/ready-to-wear/our-categories/knitwear/crew-top-multicor-nero-PUL0118KNI075VA152BL01.html"
+  },
+
+]
